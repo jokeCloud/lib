3e40b3a78ea7e831a8ce5ed0e7ae66e2caf10aed
jokecloud <medalhaemerson@gmail.com>
2022-08-04 11:46:50 -0300

    mock

--- libs/test_spam/test_github_api.py
+++ libs/test_spam/test_github_api.py
@@ -1,14 +1,27 @@
 from unittest.mock import Mock
 
+import pytest
 from avatar_photo import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/14204394?v=4'
     resp_mock.json.return_value = {
         'login': 'jokeCloud', 'id': 14204394,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/14204394?v=4',
+        'avatar_url': url,
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('avatar_photo.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('jokeCloud')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('jokeCloud')
     assert 'https://avatars.githubusercontent.com/u/14204394?v=4' == url
